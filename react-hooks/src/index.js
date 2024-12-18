44a9989f98fc6b337848cb538b0165d3435e4f8a
casti110 <cupra@LAPTOP-NI3DAOF4>
2024-12-18 17:28:44 +0100

    reactHooks

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,47 +1,76 @@
 
 
-import React, {useReducer, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const initialState ={
-  message: "hi"
-};
-
-function reducer(state, action){
-  switch(action.type) {
-    case "yell":
-      return {
-        message: `HEY I JUST SAID ${state.message}`
-      };
-    case "whisper:":
-      return { 
-        message: `excuse me, i just daid ${state.message}`}
-      };
-  }
-
-
-
-
 
 function App() {
-  
-  const [state, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-  
-  return (
-    <div>
-      <p>Message: {state.message}</p>
-      <button onClick={()=> dispatch({type: "yell"})}>
-        YELL</button>
-        <button onClick={()=> dispatch({type: "whisper"})}>whisper</button>
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users')
+    .then(response => response.json())
+    .then(setData);
+  },[])
+
+  //const [status, setStatus] = useState("Not delivered");
+ //const [checked, setChecked] = useState(false);
+
+//  const [name, setName] = useState("Jan");
+//  const [admin, setAdmin] = useState(false);
+
+//  useEffect(() => {
+//     document.title = `Celebrate ${name}`
+//  }, [name]); // empty array, it means effect will only fires once on first render
+
+// useEffect(() => {
+//     console.log(`the user is: ${admin ? "admin" : "not admin"}`)
+// }, [admin])
+
+if(data) {
+
+
+  return (
+//     // <div>
+//     //   <h1>The package is:{status}</h1>
+//     //   <button onClick={() => setStatus("delivered")}>Deliver</button>
+
+      
+    // </div>
+    //  <div>
+    //   <input 
+    //   type="checkbox" 
+    //   value={checked}
+    //   onChange={() => setChecked((checked) => !checked)}
+    //   ></input>
+    //   <p>{checked ? "checked" : "not checked"}</p>
+    //  </div>
+
+    // <section>
+    //   <p>Congratulations! {name}!</p>
+    //   <button onClick={() => setName("Will")}>
+    //     Change Winner
+    //   </button>
+    //   <p>{admin ? "logged in" : "not logged in"}</p>
+    //   <button onClick={() => setAdmin(true)}>
+        
+    //     Log in
+    //     </button>
+    // </section>
+    <div>
+    <ul>
+      {data.map((user) => (
+        <li key={user.id}>{user.login}</li>
+      ))}
+    </ul>
+    <button onClick={() => setData([])}>Remove data </button>
     </div>
+  
   );
 }
-
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode> 
